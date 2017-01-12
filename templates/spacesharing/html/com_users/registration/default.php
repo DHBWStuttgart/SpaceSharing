438f5430d6d8848d3e95c24912c1961866df4c81
master-ger <huttisim@googlemail.com>
2017-01-12 12:40:23 +0100

    unötiges gelöscht
    by SH

---
+++ templates/spacesharing/html/com_users/registration/default.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * @package     Joomla.Site
+ * @subpackage  com_users
+ *
+ * @copyright   Copyright (C) 2005 - 2016 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+JHtml::_('behavior.formvalidator');
+?>
+<div class="registration<?php echo $this->pageclass_sfx?>">
+	<?php if ($this->params->get('show_page_heading')) : ?>
+		<div class="page-header">
+			<h1><?php echo $this->escape($this->params->get('page_heading')); ?></h1>
+		</div>
+	<?php endif; ?>
+
+	<form id="member-registration" action="<?php echo JRoute::_('index.php?option=com_users&task=registration.register'); ?>" method="post" class="form-validate form-horizontal well" enctype="multipart/form-data">
+		<?php // Iterate through the form fieldsets and display each one. ?>
+		<?php foreach ($this->form->getFieldsets() as $fieldset): ?>
+			<?php $fields = $this->form->getFieldset($fieldset->name);?>
+			<?php if (count($fields)):?>
+				<fieldset>
+				<?php // If the fieldset has a label set, display it as the legend. ?>
+				<?php if (isset($fieldset->label)): ?>
+					<legend><?php echo JText::_($fieldset->label);?></legend>
+				<?php endif;?>
+				<?php // Iterate through the fields in the set and display them. ?>
+				<?php foreach ($fields as $field) : ?>
+					<?php // If the field is hidden, just display the input. ?>
+					<?php if ($field->hidden): ?>
+						<?php echo $field->input;?>
+					<?php else:?>
+						<div class="form-group">							
+							<?php echo $field->label; ?>
+							<?php if (!$field->required && $field->type != 'Spacer') : ?>
+								<span class="optional"><?php echo JText::_('COM_USERS_OPTIONAL');?></span>
+							<?php endif; ?>
+							
+							<div class="col-sm-10">
+								<?php echo $field->input;?>
+							</div>
+						</div>
+					<?php endif;?>
+				<?php endforeach;?>
+				</fieldset>
+			<?php endif;?>
+		<?php endforeach;?>
+		<div class="control-group">
+			<div class="controls">
+				<button type="submit" class="btn btn-primary validate"><?php echo JText::_('JREGISTER');?></button>
+				<a class="btn" href="<?php echo JRoute::_('');?>" title="<?php echo JText::_('JCANCEL');?>"><?php echo JText::_('JCANCEL');?></a>
+				<input type="hidden" name="option" value="com_users" />
+				<input type="hidden" name="task" value="registration.register" />
+			</div>
+		</div>
+		<?php echo JHtml::_('form.token');?>
+	</form>
+	
+	
+	<form class="form-horizontal">
+  <div class="form-group">
+    <label for="inputEmail3" class="col-sm-2 control-label">Email</label>
+    <div class="col-sm-10">
+      <input type="email" class="form-control" id="inputEmail3" placeholder="Email">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputPassword3" class="col-sm-2 control-label">Password</label>
+    <div class="col-sm-10">
+      <input type="password" class="form-control" id="inputPassword3" placeholder="Password">
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <div class="checkbox">
+        <label>
+          <input type="checkbox"> Remember me
+        </label>
+      </div>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="col-sm-offset-2 col-sm-10">
+      <button type="submit" class="btn btn-default">Sign in</button>
+    </div>
+  </div>
+</form>
+	
+	
+</div>
