d0e5343a403ee8854a023f3dfba9b5e19b559f09
Simon <huttisim@googlemail.com>
2017-01-13 10:08:00 +0100

    Facbeook Image in Templateordner verschoben
    by SH

--- templates/spacesharing/index.php
+++ templates/spacesharing/index.php
@@ -200,7 +200,7 @@ include_once JPATH_THEMES.'/'.$this->template.'/logic.php';
 						<div class="col-lg-4 col-md-4 col-sm-4 col-xs-12 footer two">
 							<h3>Folge uns </h3>
 							
-							<a href="https://de-de.facebook.com/reallabor.spacesharing/" target="_blank"><img src="images/facebook.png" alt="SpaceSharing"/></a>
+							<a href="https://de-de.facebook.com/reallabor.spacesharing/" target="_blank"><img src="<?php echo $tpath ?>/images/facebook.png" alt="SpaceSharing"/></a>
 
 							<p> </p>
 							<iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2Freallabor.spacesharing%2F&width=450&layout=standard&action=like&size=large&show_faces=false&share=false&height=35&colorscheme=dark&appId" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true"></iframe>
